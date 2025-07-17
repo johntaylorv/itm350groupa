e20ccc70a28c721714e2f8f467ceb15f2ed1a122
johntaylorv <johntaylorv@gmail.com>
2025-07-16 19:43:12 -0600

    Minor Python Tweaks

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,8 +1,9 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
-import pytest
 from src.app import app
+import pytest
+
 
 @pytest.fixture
 def client():
